427d27e5ab2b38191d189d1af6f8c397bca7039c
Nagle Zhang <nagle.zhang@qq.com>
2022-09-07 09:44:57 +0000

    finish home work

--- code/week01/src/MyLib.hs
+++ code/week01/src/MyLib.hs
@@ -1,10 +1,13 @@
 
 -- Write a function that takes a value and multiplies it by 3. Test that it works using GHCi.
-
+mult3 x = x*3 
 -- Write a function that calculates the area of a circle. Test that it works using GHCi.
+calcCircle r = pi * r ^ 2
 
--- Write a function that calculates the volume of a cylinder by composing the previous function together with the height of the cylinder. 
+-- Write a function that calculates the volume of a cylinder by composing the previous function together with the height of the cylinder.
+calcCylinder r h =  calcCircle r * h
+
 -- Test that it works using GHCi.
 
 -- Write a function that checks if the volume of a cylinder is greater than or equal to 42. Test that it works using GHCi.
-
+greaterThen42 r h = calcCylinder r h >= 42
